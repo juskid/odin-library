c3de58ed4c81604fd6276bc6263a9385d35b567c
juskid <justinkidchob@gmail.com>
2024-10-09 23:14:23 -0400

    add form allow library to update publish books

--- main.js
+++ main.js
@@ -1,46 +1,61 @@
+//var
+
+const form = {
+    bookTitle: document.querySelector("#book-title"),
+    bookAuthor: document.querySelector("#book-author"),
+    bookPages: document.querySelector("#book-pages"),
+    addBtn: document.querySelector("#addBtn"),
+};
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+};
+
 const testObject = {
     title: 'title',
     author: 'author',
     pages: 69,
     read: true,
-}
+};
 
-const myLibrary = [testObject];
+let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    //code
-}
+form.addBtn.addEventListener('click', addBookToLibrary);
+const grid = document.querySelector('.library-grid');
+//method
+
 
 function addBookToLibrary() {
-
+    let book = new Book(form.bookTitle.value, form.bookAuthor.value, form.bookPages.value);
+    myLibrary.push(book);
+    displayLibrary();
 }
 
 function displayLibrary() {
-    //foreach
-    const bookContainer = document.createElement("div");
-    const title = document.createElement('div');
-    const author = document.createElement('div');
-    const pages = document.createElement('div');
-    const read = document.createElement('div');
+    //clear grid
+    grid.innerHTML = "";
+    myLibrary.forEach((book) => {
+        let bookContainer = document.createElement("div");
+        let title = document.createElement('div');
+        let author = document.createElement('div');
+        let pages = document.createElement('div');
 
-    title.textContent = `Title: ${testObject.title}`;
-    author.textContent = `Author: ${testObject.author}`;
-    pages.textContent = `Pages: ${testObject.pages}`;
-    read.textContent = `Read: ${testObject.read}`;
+        title.textContent = `Title: ${book.title}`;
+        author.textContent = `Author: ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
 
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(pages);
 
-    bookContainer.appendChild(title);
-    bookContainer.appendChild(author);
-    bookContainer.appendChild(pages);
-    bookContainer.appendChild(read);
+        bookContainer.classList.add('bookCard');
 
-    bookContainer.classList.add('bookCard');
+        grid.appendChild(bookContainer);
+    });
 
-    const grid = document.querySelector('.library-grid');
-
-
-
-    grid.appendChild(bookContainer);
+    console.log('display');
 }
 
 displayLibrary();
